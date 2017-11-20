af85b29422616c4bf05b186d6566f4696f195bb0
matt10456 <emeta01@yahoo.fr>
2017-11-20 10:07:00 +0100

    Dossier courbes et première detection du click dans le canevas

---
+++ graphicCurve/js.js
@@ -0,0 +1,178 @@
+ var canvas, ctx;
+var p0 = {x: 0, y: 100}; //use whatever points you want obviously
+var p1 = {x: 50, y: 100}; // tan
+var p2 = {x: 50, y: 0}; // tan
+var p3 = {x: 100, y: 0};
+var accuracy = 0.1; //this'll give the bezier 100 segments
+window.onload = init;
+
+
+function init() {
+  canvas = document.querySelector('#myCanvas');
+  ctx = canvas.getContext('2d');
+  
+  canvas.addEventListener('click',function(evt){ getPos(evt); },false);
+  
+  drawCurve();
+  updateAll();
+}
+
+function getPos(evt) {
+  //alert(evt.clientX + ',' + evt.clientY);
+  // click a gauche = modifier p0 et p1
+  // click a droite = modifier p2 et p3
+  var x = evt.clientX - canvas.offsetLeft;
+  var y = evt.clientY - canvas.offsetTop;
+  if(x < (canvas.width/2)) {
+       p0.y = y;
+       p1.y = y;
+    }
+  else {
+      p2.y = y;
+      p3.y = y;
+    }
+  
+  updateAll();
+  drawCurve();
+}
+
+
+            
+function bezier(t, p0, p1, p2, p3){
+    var cX = 3 * (p1.x - p0.x),
+        bX = 3 * (p2.x - p1.x) - cX,
+        aX = p3.x - p0.x - cX - bX;
+            
+    var cY = 3 * (p1.y - p0.y),
+        bY = 3 * (p2.y - p1.y) - cY,
+        aY = p3.y - p0.y - cY - bY;
+            
+    var x = (aX * Math.pow(t, 3)) + 
+              (bX * Math.pow(t, 2)) + (cX * t) + p0.x;
+    var y = (aY * Math.pow(t, 3)) + 
+              (bY * Math.pow(t, 2)) + (cY * t) + p0.y;
+            
+    return {x: x, y: y};
+} 
+  
+function changeBiasX(val) {
+  p2.x = val;
+  drawCurve(); 
+} 
+
+function changeBiasY(val) {
+  p1.x = val;
+  drawCurve();
+  
+   
+
+}
+
+function squeeze(val) {
+  val = parseFloat(val);
+  val1 = map(val, 0, 10, 0, 50);
+  p2.y = val1;
+  val2 = map(val, 0, 10, 100, 50);
+  p1.y = val2;
+  drawCurve();
+  
+   
+}
+
+function changeStartY2(val) {
+  p3.y = val;
+  p2.y = val;
+  drawCurve();
+  
+ 
+}
+
+
+
+function changeStartY1(val) {
+  p0.y = parseInt(val);
+  p1.y = val;
+  drawCurve();
+  
+ 
+}
+
+
+function changeK(val) {
+  val = parseFloat(val);
+  // first map k to [0, 100]
+  var k1 = map(val, 0, 10, 100, 0);
+  changeBiasX(k1);
+  var k2 = map(val, 0, 10, 0, 100);
+  changeBiasY(k2);
+}
+
+function changeSmaller(val) {
+  val = parseFloat(val);
+  var startY1 = map(val, 0, 10, 100, 50);
+  changeStartY1(startY1);
+  var startY2 = map(val, 0, 10, 0, 50);
+  changeStartY2(startY2);
+} 
+
+function changeBias(val) {
+  val = parseFloat(val);
+  if(val <= 5) {
+    var sx = map(val, 0, 5, 0, 50);
+    changeBiasX(sx);
+
+  } else if(val > 5) {
+    var sy = map(val, 5, 10, 50, 100);
+    changeBiasY(sy);
+  }
+}
+
+function drawCurve() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  ctx.beginPath();
+  ctx.moveTo(p0.x, p0.y);
+  // i entre 0 et 1
+  for (var i=0; i<1; i+=accuracy){
+      var p = bezier(i, p0, p1, p2, p3);
+      ctx.lineTo(p.x, p.y);
+  }
+  ctx.stroke();
+  
+  updateAll();
+}
+
+function returnCurve() {
+   var n_samples = 44100,
+   accuracy = 1/n_samples,
+   curve = new Float32Array(n_samples),
+   index = 0;
+  
+  curve[index++] = map(p0.y, 0, 100, -1, 1);
+  
+  // 
+  for (var i=0; i<1; i+=accuracy){
+    var p = bezier(i, p0, p1, p2, p3);
+    curve[index++] = map(p.y, 0, 100, -1, 1);
+  }
+}
+ 
+// maps a value from [istart, istop] into [ostart, ostop]
+function map(value, istart, istop, ostart, ostop) {
+    return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
+}
+
+// Update p values
+function updateAll() {
+  updatePValue(p0, 0);
+  updatePValue(p1, 1);
+  updatePValue(p2, 2);
+  updatePValue(p3, 3);
+}
+
+function updatePValue(p, index) {
+  var spanPx = document.querySelector("#" + "p"+index+"x");
+  spanPx.innerHTML = p.x;
+   var spanPy = document.querySelector("#" + "p"+index+"y");
+  spanPy.innerHTML = p.y;
+}
